b90292fa880d11b55f0c373e51b1667124b9a676
Sanjay-Sreedhar <sanjaysreedhar51@gmail.com>
2023-02-20 20:05:42 +0530

    completed a simple bmi calculator using node js and express js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 //index of js
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app=express();
+
+
+app.use (bodyParser.urlencoded({extended:true}));
+
+app.get("/",(req,res)=>{
+
+        res.sendFile(__dirname+"/index.html");
+
+});
+
+app.post("/",(req,res)=>{
+
+    var h=parseInt(req.body.height);
+    var w=parseInt(req.body.weight);
+    var bmi=w/(h*h);
+    console.log(bmi);
+
+    res.send("<h1 style='text-align:center; color:lime; '>Your BMI is:  </h1>"+bmi);
+});
+
+app.listen(8000,()=>{
+    console.log("Server running in port 8000");
+});
